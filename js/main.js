32d79b5edee28a355b931b3e22ded2661d854b17
Stas Nechay <stanislav.nechay@gmail.com>
2018-02-17 15:23:38 +0200

    initial

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	 $('.ba-slides').slick({
+	 	  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  centerMode: false,
+  variableWidth: true
+});
+
+
+});
+
+
